1783b964aa3a8c2bd025af835067ce0f0a71bf82
Mariia Nekrasova <150371132+Kowoya@users.noreply.github.com>
2025-09-20 15:38:44 +0300

    Add structures: Character, Enemy, PossessedEnemy, Player, and Item

--- Dominion Of Shadows/Dominion Of Shadows.cpp
+++ Dominion Of Shadows/Dominion Of Shadows.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
-//It's a testing code brench
+//It's a dev code brench
+
+struct Character {
+	string name;
+	string affiliation;
+	int sanity;
+};
+
+struct Player {
+	int ritualKnowledge;
+
+};
+
+struct Enemy {
+	string name;
+	int damage;
+	int aggressiveness;
+	int lvl;
+};
+
+struct PossessedEnemy {
+	string demonName;
+	string humanName;
+	int possessionStrength;
+	int demonLvl;
+};
+
+struct Item {
+	int weight;
+	int rarity;
+};
 
 int main() {
 	cout << "Hello in \"Dominion Of Shadows\". Enjoy the game.";
